79a548733fc17a4c603ae98c7f4ffc8171e59e2e
Your Name <you@example.com>
2025-11-27 13:27:20 +0530

    lecture-8

---
+++ Lecture-8-Practical/spring_framework_demo/src/test/java/com/gfg_pratical/spring_framework_demo/SpringFrameworkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfg_pratical.spring_framework_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
